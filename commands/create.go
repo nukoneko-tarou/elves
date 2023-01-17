a3b69d5cdcb4825e319d1b5411c4fc5057846f3d
nukoneko-tarou <nukonekotarou@gmail.com>
2023-01-17 14:09:06 +0900

    add flags

--- commands/create.go
+++ commands/create.go
@@ -8,6 +8,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -34,22 +36,58 @@ func NewCreate() *Create {
 	cmd := &Create{}
 	cmd.Cmd = &cobra.Command{
 		Use:   "create",
-		Short: "create",
-		Long:  `create`,
+		Short: "Create a directory",
+		Long:  `Creates a directory where the command is executed`,
+		Args:  cobra.MinimumNArgs(1),
 		RunE:  cmd.run,
+		Example: strings.Join([]string{
+			"elves create ./sample.json",
+			"elves create ./sample.json --sub new-project --permission 777",
+		}, "\n"),
 	}
+
+	cmd.Cmd.Flags().StringP("permission", "p", "", "permission")
+	cmd.Cmd.Flags().StringP("sub", "s", "", "Create in subdirectory")
 
 	return cmd
 }
 
-func (c *Create) run(cmd *cobra.Command, _ []string) error {
-	tree, err := c.perseJson()
+func (c *Create) run(cmd *cobra.Command, args []string) error {
+	permission := "755"
+	subDirectoryName := "./"
+
+	p, _ := cmd.Flags().GetString("permission")
+	sn, _ := cmd.Flags().GetString("sub")
+
+	if p != "" {
+		permission = p
+	}
+
+	if sn != "" {
+		path := "./" + sn
+		perm32, _ := strconv.ParseUint(permission, 8, 32)
+		err := os.Mkdir(path, os.FileMode(perm32))
+		if err != nil {
+			fmt.Println(err.Error())
+			os.Exit(1)
+		}
+
+		subDirectoryName = path
+	}
+
+	raw, err := ioutil.ReadFile(args[0])
 	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(1)
 	}
 
-	err = c.createDirectory(tree[0].Contents, "./")
+	tree, err := c.perseJson(raw)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	err = c.createDirectory(tree[0].Contents, subDirectoryName, permission)
 	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(1)
@@ -58,14 +96,8 @@ func (c *Create) run(cmd *cobra.Command, _ []string) error {
 	return nil
 }
 
-func (c *Create) perseJson() (TreeJson, error) {
-	raw, err := ioutil.ReadFile("./sample.json")
-	if err != nil {
-		return nil, err
-	}
-
+func (c *Create) perseJson(raw []byte) (TreeJson, error) {
 	var fc TreeJson
-
 	if err := json.Unmarshal(raw, &fc); err != nil {
 		return nil, err
 	}
@@ -73,17 +105,19 @@ func (c *Create) perseJson() (TreeJson, error) {
 	return fc, nil
 }
 
-func (c *Create) createDirectory(contents []DirectoryJson, parentName string) error {
+func (c *Create) createDirectory(contents []DirectoryJson, parentName string, permission string) error {
 	for _, v := range contents {
 		if v.Type == "directory" {
 			path := parentName + "/" + v.Name
-			if err := os.Mkdir(path, 0777); err != nil {
+			perm32, _ := strconv.ParseUint(permission, 8, 32)
+
+			if err := os.Mkdir(path, os.FileMode(perm32)); err != nil {
 				fmt.Println(err.Error())
 				os.Exit(1)
 			}
 
 			if len(v.Contents) != 0 {
-				c.createDirectory(v.Contents, path)
+				c.createDirectory(v.Contents, path, permission)
 			}
 		}
 	}
